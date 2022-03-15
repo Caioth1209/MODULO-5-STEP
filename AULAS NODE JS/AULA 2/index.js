4441ad2b2e2b1cd846185a2e841a722c3bf1b641
Caio Araujo <caiodaluz2004@gmail.com>
2022-03-15 20:27:54 -0300

    Subindo aula 2

---
+++ AULAS NODE JS/AULA 2/index.js
@@ -0,0 +1,53 @@
+// como se fosse import
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// responsavel por pegar os dados
+const fetchData = async (url) => {
+
+    // axios para pegar os dados
+    const result = await axios.get(url);
+    return result.data;
+    
+} 
+
+const main = async () => {
+    const content = await fetchData("https://stardewvalleywiki.com/Villagers");
+
+    // cheerio para fazer a raspagem
+    const $ = cheerio.load(content);
+
+    let villagers = [];
+
+    $("li.gallerybox").each((i, el)=>{
+        const title = $(el).find(".gallerytext > p > a").text();
+
+        const data = {title};
+
+        villagers.push(data);
+    })
+
+    console.log({villagers});
+}
+
+main();
+
+
+
+
+
+
+
+
+
+// var http = require('http');
+
+// http.createServer((request, response)=>{
+//     response.writeHead(200,{"Content-type":"text/html"});
+
+//     response.write(
+//         "<h1>Funcionou legal</h1>"
+//     );
+
+//     response.end();
+// }).listen(8080);
