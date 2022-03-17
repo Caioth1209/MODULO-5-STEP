13a1e004d232f4073ee6ed20f00a59aa39239ad2
Caio Araujo <caiodaluz2004@gmail.com>
2022-03-17 20:41:52 -0300

    Aula 3

---
+++ AULAS NODE JS/AULA 3/config/routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const routes = express.Router();
+
+let db = [{
+    "1" : {
+        nome: "Alan",
+        idade: "30"
+    }
+}]
+
+// f1 - get
+// param1 - path da rota
+// param2 - function retornando req e resp
+
+routes.get("/", (req, resp)=>{
+    return resp.json(db);
+})
+
+// f2 - post
+// param1 - path da rota
+// param2 - function retornando req e resp
+
+routes.post("/add", (req, resp)=>{
+
+    // passa o objeto json para o body
+    const body = req.body;
+
+    // se tiver problema, retorna erro
+    if (!body) {
+        return resp.status(400).end;
+    }
+
+    // se nao, colocar dentro do db
+    db.push(body);
+
+    // retorna a resposta.
+    return resp.json(body);
+})
+
+// f3 - delete
+
+module.exports = routes;
