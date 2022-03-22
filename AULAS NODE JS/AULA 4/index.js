eed8e8b2e0f90e8b90fcc99058fac88b8abc0b57
Caio Araujo <caiodaluz2004@gmail.com>
2022-03-22 19:25:59 -0300

    att

---
+++ AULAS NODE JS/AULA 4/index.js
@@ -0,0 +1,17 @@
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAx8DS-2JR0ypbKXsAxSKA3eucXN6111qQ",
+  authDomain: "aula4nodedb.firebaseapp.com",
+  projectId: "aula4nodedb",
+  storageBucket: "aula4nodedb.appspot.com",
+  messagingSenderId: "295674346514",
+  appId: "1:295674346514:web:183a209106fc0c9bc82ccb",
+  measurementId: "G-1L0B8VWRNE"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function createDataInRealTime() {
+    
+} 
