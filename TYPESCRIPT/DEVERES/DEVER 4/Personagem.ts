e63f5c12ea5539b3b2a2831455111b4fe68763d1
Caio Araujo <caiodaluz2004@gmail.com>
2022-06-15 18:27:40 -0300

    subindo aula 4

---
+++ TYPESCRIPT/DEVERES/DEVER 4/Personagem.ts
@@ -0,0 +1,63 @@
+// para rodar o ts-node: npx ts-node <nome do arquivo>
+
+import prompt from "prompt-sync";
+
+export class Personagem{
+    constructor(
+        public nome: string,
+        public energia: number,
+        public vida: number,
+        public ataque: number,
+        public defesa: number
+    ){
+
+    } 
+}
+
+let ken : Personagem = new Personagem("Ken Masters", 10, 10, 10, 10);
+
+let teclado = prompt();
+
+let option : number = 0;
+
+while (option != 9) {
+    console.log("\n///// Personagem /////");
+    console.log("(1) - Treinar Ataque");
+    console.log("(2) - Treinar Defesa");
+    console.log("(3) - Imprimir Atributos");
+    console.log("(9) - Sair do game");
+    console.log("//////////////////////\n");
+    
+    option =+ teclado("Escolha uma ação: ");
+
+    switch (option) {
+        case 1:{
+            console.log(`\nTreinando ataque do ${ken.nome}!!!`);
+            console.log("+2 de ataque!\n");
+            ken.ataque += 2;
+            break;
+        }
+
+        case 2:{
+            console.log(`\nTreinando defesa do ${ken.nome}!!!`);
+            console.log("+2 de defesa!\n");
+            ken.defesa += 2
+            break;
+        }
+
+        case 3:{
+            console.log(ken);
+            break;
+        }
+
+        case 9:{
+            console.log("Saindo...");
+            break;
+        }
+        
+        default:{
+            console.log("Escolha errada...\n");
+            break;
+        }
+    }
+}
