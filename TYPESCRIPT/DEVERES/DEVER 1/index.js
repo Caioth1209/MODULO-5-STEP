c8e0881c382719e05e74d8eb36597f949a85d266
Caio Araujo <caiodaluz2004@gmail.com>
2022-07-26 19:08:00 -0300

    att

--- TYPESCRIPT/DEVERES/DEVER 1/index.js
+++
@@ -1,9 +0,0 @@
-var isPalindromo = function (palavra) {
-    var palavraInvertida = palavra.split("").reverse().join("");
-    // separa a string em um array
-    // inverte o array
-    // junta tudo de novo
-    return palavra == palavraInvertida;
-};
-console.log(isPalindromo("casinha"));
-console.log(isPalindromo("asa"));
