933465a171334d17bd0d5c80c14825113325604a
Caio Araujo <caiodaluz2004@gmail.com>
2022-07-28 18:12:25 -0300

    att

--- REACT-NATIVE/AULA1/App.js
+++ REACT-NATIVE/AULA1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>OI CAIO</Text>
     </View>
   )
 }
