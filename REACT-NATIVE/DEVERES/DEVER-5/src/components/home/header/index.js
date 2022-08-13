29e7ed0e3cf5841396b7c325a4540bdf89072fa6
Caio Araujo <caiodaluz2004@gmail.com>
2022-08-13 16:19:58 -0300

    Subindo dever 5

---
+++ REACT-NATIVE/DEVERES/DEVER-5/src/components/home/header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
+import styles from "./styles"
+
+export default function App(props) {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.boxImg}>
+                <Image
+                source={{uri: "https://cdn-icons-png.flaticon.com/512/5080/5080067.png"}}
+                style={styles.img}/>
+            </View>
+
+            <View style={styles.boxInfo}>
+                <Text 
+                style={styles.textInfo}>CAIO ARAUJO DA LUZ</Text>
+
+                <Text 
+                style={styles.textInfo}>174.851.637-01</Text>
+
+                <TouchableOpacity
+                style={styles.buttonAdd}
+                onPress={()=>{props.handleAddButton(parseFloat(props.moneyQuantity) + 25.50)}}>
+                    <Text
+                    style={styles.textButton}>
+                        ADD
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
---
+++ REACT-NATIVE/DEVERES/DEVER-5/src/components/home/saldo/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        borderWidth: 3,
+        borderColor: "#463f3a",
+        borderRadius: 30,
+        padding: 30
+    },
+    saldoTitulo:{
+        fontWeight: "bold",
+        fontSize: 17,
+    },
+    saldoDinheiro:{
+        fontWeight: "bold",
+        fontSize: 24,
+        textAlign: "center"
+    }
+
+})
